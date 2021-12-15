024c00cafd5ad975e10ee13b0a9b7506888c7c16
Calum Monro <calum@mrmglobal.com>
2021-12-15 13:01:13 +0000

    complete day 6-1

---
+++ day-6-1.clj
@@ -0,0 +1,33 @@
+(ns day-5-1
+  (:require [clojure.string :as str]))
+
+(def sample-data (slurp "day-6-sample-data.txt"))
+(def problem-data (slurp "day-6-problem-data.txt"))
+
+(defn parse [data]
+  (as-> data v
+      (str/split v #"\n")
+      (first v)
+      (str/split v #",")
+      (mapv #(Integer/parseInt %) v)))
+
+(defn get-next-fish-state [fish-state]
+  (if (= 0 fish-state)
+    [6 8]
+    (dec fish-state)))
+
+(defn get-next-fish-states [fish-states]
+  (vec (flatten (mapv get-next-fish-state fish-states))))
+
+(defn simulate [data]
+  (let [initial-fish-states (parse data)]
+    (loop [fish-states initial-fish-states
+           day 0]
+      (if (= day 80)
+        (count fish-states)
+        (recur (get-next-fish-states fish-states)
+               (inc day))))))
+
+(= 5934 (simulate sample-data))
+
+(simulate problem-data)
