9969fedd8512718e59e9f869e2b881c6c26866db
Calum Monro <calum.monro@live.co.uk>
2022-01-05 22:10:56 +0000

    tidy up day 7-2

--- day-7-2.clj
+++ day-7-2.clj
@@ -4,8 +4,6 @@
 (def sample-data (slurp "day-7-sample-data.txt"))
 (def problem-data (slurp "day-7-problem-data.txt"))
 
-(def positions (parse sample-data))
-
 (defn parse [data]
   (as-> data v
       (str/split v #"\n")
@@ -13,31 +11,22 @@
       (str/split v #",")
       (mapv #(Integer/parseInt %) v)))
 
-;; 1. loop from 0 - max(positions)
-;; 2. for each position,
-;;    - reduce to sum of distances from element to position
+(defn fuel-cost [x target-x]
+  (let [steps (Math/abs (- x target-x))]
+    (loop [step 1
+           cost 0]
+      (if (> step steps)
+        cost
+        (recur (inc step) (+ cost step))))))
 
-;; note after copmletion: the key for this was just to adjust this function,
-;; instead of just incrementing by the differnece, we want to calculate the fuel used for a given difference
-(defn calculate-fuel-used [delta]
-  (loop [d 1
-         fuel-used 0]
-    (if (> d delta)
-      fuel-used
-      (recur (inc d) (+ fuel-used d)))))
+(defn total-fuel-cost [positions target]
+  (->> positions
+       (map (partial fuel-cost target))
+       (reduce +)))
 
-(defn get-total-for-number [positions number]
-  (println (str "number: " number))
-  (reduce (fn [total position]
-            (let [delta (Math/abs (- position number))
-                  fuel-used (calculate-fuel-used delta)]
-              (+ total fuel-used)))
-          0
-          positions))
-
-(defn find-smallest-total [positions]
+(defn min-fuel-cost [positions]
   (let [max-position (apply max positions)
-        position-range (range max-position)]
-    (println "poop")
-    (apply min (map (partial get-total-for-number positions)
-                    position-range))))
+        targets (range max-position)]
+    (->> targets
+         (map (partial total-fuel-cost positions))
+         (apply min))))
